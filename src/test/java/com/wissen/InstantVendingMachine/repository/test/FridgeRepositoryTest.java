3a9be74c157eb87c151b90668d9072f488f68624
dikshadinesh <38341526+dikshadinesh@users.noreply.github.com>
2019-07-16 11:17:08 +0530

    Fridge entity

---
+++ src/test/java/com/wissen/InstantVendingMachine/repository/test/FridgeRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.wissen.InstantVendingMachine.repository.test;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.wissen.InstantVendingMachine.BaseTest;
+import com.wissen.instantvendingmachine.entity.FridgeEntity;
+import com.wissen.instantvendingmachine.repository.FridgeRepository;
+
+public class FridgeRepositoryTest extends BaseTest {
+
+	@Autowired
+	private FridgeRepository fridgeRepository;
+
+	@Test
+	public void testFindAll() {
+		List<FridgeEntity> fridge = fridgeRepository.findAll();
+		fridge.forEach(System.out::println);
+	}
+
+}
